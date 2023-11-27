5a24f77f51db71a7c23f3820df797e088344b79d
klaudiamilosia <klaudiamiloszewska100@gmail.com>
2023-11-27 13:55:35 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,44 +8,18 @@ module.exports = {
     extend: {
       colors: {
         dracula: {
-          100: '#FFFFFF', //big text h1's
-          150: '#f7f7f7', //big text h1's
-          200: '#efeef2', //smaller text h3's
-          300: '#E4E2E9', //grayed text p's ex. navbar
-          400: '#B3AFC0', //more grayed text ex.extra elements
+          100: '#ffffff',
+          150: '#f7f7f7',
+          200: '#efeef2',
+          300: '#E4E2E9',
+          400: '#B3AFC0',
           450: '#84818e',
-          500: '#565167', //the grayest text ex.extra info
-          600: '#2B2833', //border
-          700: '#1D1B22', //modules bg
-          800: '#18171D', //elements bg
-          900: '#0E0D11', //main bg
+          500: '#565167',
+          600: '#2B2833',
+          700: '#1D1B22', 
+          800: '#18171D',
+          900: '#0E0D11',
         },
-        midnight: {
-          50: '#F6F6F9',
-          100: '#EAEAEF',
-          150: '#E4E4E7',
-          200: '#DCDCE4',
-          300: '#C0C0CF',
-          400: '#9D9DB2',
-          500: '#8F8EA9',
-          600: '#6F6F8E',
-          650: '#666687', //border
-          700: '#4A4A6A',
-          800: '#33324D',
-          900: '#212134', //medium background
-          950: '#181826', //darkest background
-        },
-        saphire: {
-          100: '#E9EFFD',
-          200: '#BDD0FB',
-          300: '#91B3FA',
-          400: '#7D97F4',
-          500: '#6467fa',
-          600: '#4548ED',
-          700: '#1A0F90',
-          800: '#0B0161',
-          900: '#060130',
-        }
       },     
     },
   },
