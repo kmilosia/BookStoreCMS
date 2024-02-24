8532b0d738f6e72847d89662716348830237f92a
klaudiamilosia <klaudiamiloszewska100@gmail.com>
2024-02-24 14:05:51 +0100

    commit

--- src/api/selectAPI.js
+++ src/api/selectAPI.js
@@ -153,7 +153,7 @@ export const getFormBookItems = async (setData) => {
       if(response.status === 200 || response.status === 204){
       const options = response.data.map(item => ({
         value: item,
-        label: item.bookTitle + " (" + item.formName + ")"
+        label: item.bookTitle + " (" + (item.formName === 'Book' ? 'Książka' : 'Ebook') + ")"
       }))
       setData(options)
     }}
